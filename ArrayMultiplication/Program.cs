12aa27f45d7f33143ab51e811028bd1dbf2b9193
Daenstar <demonline2005@mail.ru>
2021-12-05 14:08:41 +0300

    initial commit

---
+++ ArrayMultiplication/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayMultiplication
+{
+    class Multiplicator
+    {
+        public static int[] Multiply(int[] a, int[] b)
+        {
+          
+            if (a.Length == 0 || b.Length == 0)
+            {
+                throw new Exception("Array(s) can't be empty.");
+            }
+            int[] result = new int[a.Length + b.Length - 1];
+            for (int i = 0; i < a.Length; i++)
+            {
+                for(int j = 0; j < b.Length; j++)
+                {
+                    result[i + j] += a[i] * b[j];
+                }
+            }
+            return result;
+        }
+    }
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int[] a = new int[] { -1, 1 };
+            int[] b = new int[] { 2, 1 };
+            int[] res = Multiplicator.Multiply(a, b);
+            res.ToList().ForEach(i => Console.Write(i.ToString() + " "));
+            
+        }
+    }
+}
