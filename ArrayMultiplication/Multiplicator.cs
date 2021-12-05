5f273810861d5b10ee24e8a1cf3f517679e15206
dennn93 <demonline2005@mail.ru>
2021-12-05 14:23:16 +0300

    separate Multiplicaor from Program

---
+++ ArrayMultiplication/Multiplicator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayMultiplication
+{
+    public class Multiplicator
+    {
+        public static int[] Multiply(int[] a, int[] b)
+        {
+
+            if (a.Length == 0 || b.Length == 0)
+            {
+                throw new Exception("Array(s) can't be empty.");
+            }
+            int[] result = new int[a.Length + b.Length - 1];
+            for (int i = 0; i < a.Length; i++)
+            {
+                for (int j = 0; j < b.Length; j++)
+                {
+                    result[i + j] += a[i] * b[j];
+                }
+            }
+            return result;
+        }
+    }
+}
--- ArrayMultiplication/Program.cs
+++ ArrayMultiplication/Program.cs
@@ -6,26 +6,7 @@ using System.Threading.Tasks;
 
 namespace ArrayMultiplication
 {
-    class Multiplicator
-    {
-        public static int[] Multiply(int[] a, int[] b)
-        {
-          
-            if (a.Length == 0 || b.Length == 0)
-            {
-                throw new Exception("Array(s) can't be empty.");
-            }
-            int[] result = new int[a.Length + b.Length - 1];
-            for (int i = 0; i < a.Length; i++)
-            {
-                for(int j = 0; j < b.Length; j++)
-                {
-                    result[i + j] += a[i] * b[j];
-                }
-            }
-            return result;
-        }
-    }
+   
     class Program
     {
         
