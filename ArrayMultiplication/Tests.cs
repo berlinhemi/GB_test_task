27fc493adb6445b0b013b1c221f0c6b1b1469921
dennn93 <demonline2005@mail.ru>
2021-12-05 20:34:18 +0300

    added some tests for arrays with trailing zeros

--- ArrayMultiplication/Tests.cs
+++ ArrayMultiplication/Tests.cs
@@ -10,7 +10,7 @@ namespace ArrayMultiplication
     {
         public static void RunAllTests()
         {
-            int[] result = Multiplicator.Multiply(new int[] { -1, 1 }, new int[] { 2, 1 });
+            int[] result = Multiplicator.MultiplyArrays(new int[] { -1, 1 }, new int[] { 2, 1 });
             int[] right_answer = new int[] { -2, 1, 1 };
             if (Enumerable.SequenceEqual(result, right_answer))
             {
@@ -22,7 +22,7 @@ namespace ArrayMultiplication
             }
 
 
-            result = Multiplicator.Multiply(new int[] { 1, 1, 1 }, new int[] { 1, 1, 1 });
+            result = Multiplicator.MultiplyArrays(new int[] { 1, 1, 1 }, new int[] { 1, 1, 1 });
             right_answer = new int[] { 1, 2, 3, 2, 1 };
             if (Enumerable.SequenceEqual(result, right_answer))
             {
@@ -34,7 +34,7 @@ namespace ArrayMultiplication
             }
 
 
-            result = Multiplicator.Multiply(new int[] { 1, 1, 1 }, new int[] { 0, 0, 1, 1 });
+            result = Multiplicator.MultiplyArrays(new int[] { 1, 1, 1 }, new int[] { 0, 0, 1, 1 });
             right_answer = new int[] { 0, 0, 1, 2, 2, 1 };
             if (Enumerable.SequenceEqual(result, right_answer))
             {
@@ -46,32 +46,35 @@ namespace ArrayMultiplication
             }
 
 
-            try
-            {
-                Multiplicator.Multiply(new int[] { 1, 1, 1 }, new int[] { 1, 1, 0, 0 });
-                Console.WriteLine("Test4 FAILED");
-            }
-            catch
+            result = Multiplicator.MultiplyArrays(new int[] { 1, 1, 1 }, new int[] { 0, 0, 1, 1, 0, 0, 0 });
+            right_answer = new int[] { 0, 0, 1, 2, 2, 1 };
+            if (Enumerable.SequenceEqual(result, right_answer))
             {
                 Console.WriteLine("Test4 OK");
             }
-            
-
-
-            try
+            else
             {
-                Multiplicator.Multiply(new int[] { }, new int[] { 2, 1 });
-                Console.WriteLine("Test5 FAILED");
+                Console.WriteLine("Test4 FAILED");
             }
-            catch
+
+
+
+            result = Multiplicator.MultiplyArrays(new int[] { 1, 1, 1 }, new int[] { 1, 1, 0, 0 });
+            right_answer = new int[] {1, 2, 2, 1 };
+            if (Enumerable.SequenceEqual(result, right_answer))
             {
                 Console.WriteLine("Test5 OK");
             }
+            else
+            {
+                Console.WriteLine("Test5 FAILED");
+            }
+
 
 
             try
             {
-               Multiplicator.Multiply(new int[] {1,2,3 }, new int[] {});
+                Multiplicator.MultiplyArrays(new int[] { }, new int[] { 2, 1 });
                 Console.WriteLine("Test6 FAILED");
             }
             catch
@@ -82,12 +85,23 @@ namespace ArrayMultiplication
 
             try
             {
-                Multiplicator.Multiply(new int[] {  }, new int[] { });
+               Multiplicator.MultiplyArrays(new int[] {1,2,3 }, new int[] {});
                 Console.WriteLine("Test7 FAILED");
             }
             catch
             {
                 Console.WriteLine("Test7 OK");
+            }
+
+
+            try
+            {
+                Multiplicator.MultiplyArrays(new int[] {  }, new int[] { });
+                Console.WriteLine("Test8 FAILED");
+            }
+            catch
+            {
+                Console.WriteLine("Test8 OK");
             }
 
         }
