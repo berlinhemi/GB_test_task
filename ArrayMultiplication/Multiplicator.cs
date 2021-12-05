ffe8f7cff2f64903a3f259e92a51e7bde874d56d
dennn93 <demonline2005@mail.ru>
2021-12-05 20:33:18 +0300

    added processing of arrays with zeros at the end

--- ArrayMultiplication/Multiplicator.cs
+++ ArrayMultiplication/Multiplicator.cs
@@ -8,18 +8,61 @@ namespace ArrayMultiplication
 {
     public class Multiplicator
     {
-        public static int[] Multiply(int[] a, int[] b)
+        /// <summary>
+        /// Вычисляет количество элементов массива за исключением последних нулевых
+        /// [1,2,0,6,0,0] -> 4
+        /// [0,1,4,3,4,0] -> 5
+        /// [1,2,3,4] -> 4
+        /// </summary>
+        /// <param name="array">массив</param>
+        /// <returns>Длина массива минус кол-во нулевых элементов в конце (если есть)  </returns>
+        private static int GetRealArrayLength(int[] array)
+        {
+            for (int i = array.Length - 1; i >= 0; i--)
+            {
+                if (array[i] != 0)
+                {
+                    return i+1;
+                }
+            }
+            return 0;
+        }
+
+        /// <summary>
+        /// Отрезает нули с конца массива (в случае наличия таковых)
+        /// </summary>
+        /// <param name="array">массив</param>
+        /// <returns>Массив</returns>
+        private static int[] RemoveTrailingZeros(int[] array)
+        {
+            int realLength = GetRealArrayLength(array);
+            if(realLength != array.Length)
+            {
+                int[] reduced = new int[realLength];
+                for(int i=0; i< realLength; i++)
+                {
+                    reduced[i] = array[i];
+                }
+                return reduced;        
+            }
+            return array;
+        }
+
+        /// <summary>
+        /// Перемножение массивов
+        /// </summary>
+        /// <param name="a">первый массив</param>
+        /// <param name="b">второй массив</param>
+        /// <returns>Результирующий массив</returns>
+        public static int[] MultiplyArrays(int[] a, int[] b)
         {
 
             if (a.Length == 0 || b.Length == 0)
             {
                 throw new Exception("Array(s) can't be empty.");
             }
-            //Нет необходимости показывать, что члены с более высокими показателями степенями имеют нулевые коэффициенты.
-            if (a[a.Length-1] == 0 || b[b.Length-1] == 0)
-            {
-                throw new Exception("Array(s) must not contain zeros at the end.");
-            }
+            a = RemoveTrailingZeros(a);
+            b = RemoveTrailingZeros(b);
             int[] result = new int[a.Length + b.Length - 1];
             for (int i = 0; i < a.Length; i++)
             {
